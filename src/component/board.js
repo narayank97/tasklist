cabe6db5b20e6c4202b933690634f14607324cf3
Karun Narayan <narayankarun@gmail.com>
2023-09-12 11:47:17 -0700

    feat : adding board check

--- src/component/board.js
+++ src/component/board.js
@@ -6,7 +6,9 @@ function Board() {
   const boardSize = 3;
   const [board, setBoard] = useState([]);
   const [turn, setTurn] = useState(0);
+  const [winner, setWinner] = useState(false);
   const icons = { 0: 'X', 1: 'O' };
+
 
   useEffect(() => {
     let newBoard = [];
@@ -23,12 +25,62 @@ function Board() {
       board[row][col] = icons[iconKey];
       setBoard(board);
       setTurn(turn + 1);
+      setWinner(winCheck(board));
       return
+    }
+  }
+
+  const winCheck = (board) => {
+    for (let i = 0; i < boardSize; i++) {
+      for (let j = 0; j < boardSize; j++) {
+        let currentPlayer = board[i][j];
+        console.log("🚀🚀🚀🚀🚀 ~ winCheck ~ currentPlayer:", currentPlayer)
+        if (currentPlayer) {
+          let currentRow = i;
+          let currentCol = j;
+          let verticalCheck = 0;
+          let horizontalCheck = 0;
+          let diagnolCheck = 0;
+
+          //vertical check
+          while (currentPlayer === board[currentRow][currentCol] && currentRow < boardSize) {
+            currentRow += 1;
+            verticalCheck += 1;
+            if (verticalCheck === boardSize) {
+              return true;
+            }
+          }
+
+          //horizontal check
+          while (currentPlayer === board[currentRow][currentCol] && currentCol < boardSize) {
+            currentCol += 1;
+            horizontalCheck += 1;
+            if (horizontalCheck === boardSize) {
+              return true;
+            }
+          }
+
+          //diagnol check
+          while (currentPlayer === board[currentRow][currentCol] && currentCol < boardSize && currentRow < boardSize) {
+            currentCol += 1;
+            currentRow += 1;
+            diagnolCheck += 1;
+            if (diagnolCheck === boardSize) {
+              return true;
+            }
+          }
+
+        }
+
+      }
     }
   }
 
   return (
     <div>
+      {
+        winner && alert("We Have a winner!")
+      }
       {board &&
         board.map((row, rowIndex) => {
           return (
